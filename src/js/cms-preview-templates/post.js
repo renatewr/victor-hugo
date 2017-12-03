8939cc915499a10d3b13d417e153f1335420e012
Renate Winther Ravnaas <renatewr@gmail.com>
2017-12-03 20:51:02 +0100

    Upgrading and using netlify auth

--- src/js/cms-preview-templates/post.js
+++ src/js/cms-preview-templates/post.js
@@ -5,19 +5,19 @@ export default class PostPreview extends React.Component {
   render() {
     const {entry, widgetFor} = this.props;
 
-    return <div className="mw6 center ph3 pv4">
-      <h1 className="f2 lh-title b mb3">{ entry.getIn(["data", "title"])}</h1>
+    return `<div className="mw6 center ph3 pv4">
+      <h1 className="f2 lh-title b mb3">${ entry.getIn(["data", "title"])}</h1>
       <div className="flex justify-between grey-3">
-        <div style={{
+        <div style=${{
           width: "80px",
           height: "80px"
         }}></div>
-        <p>{ format(entry.getIn(["data", "date"]), "ddd, MMM D, YYYY") }</p>
+        <p>${ format(entry.getIn(["data", "date"]), "ddd, MMM D, YYYY") }</p>
         <p>Read in x minutes</p>
       </div>
       <div className="cms mw6">
-        { widgetFor("body") }
+        ${ widgetFor("body") }
       </div>
-    </div>;
+    </div>`;
   }
 }
